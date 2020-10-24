0d2adaf58d714532d407db81349c776b0975b450
Marr11317 <remimarche@gmail.com>
2020-10-24 03:18:27 -0400

    remove x pakcages

--- packages/x-cli/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { awesomeFn } from "@quramy/x-core";
-
-export async function main() {
-  // dependencies across child packages
-  const out = await awesomeFn();
-  return out;
-}
--- packages/x-core/src/index.ts
+++
@@ -1,3 +0,0 @@
-export function awesomeFn() {
-  return "Hello";
-}
